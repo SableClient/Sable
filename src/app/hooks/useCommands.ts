9275e5fdf78d9baef47bb77ac4d48cf6205865f7
Shea Duma <nu@she-a.eu>
2026-03-24 16:21:08 +0200

    act made the word more general

--- src/app/hooks/useCommands.ts
+++ src/app/hooks/useCommands.ts
@@ -1516,8 +1516,7 @@ export const useCommands = (mx: MatrixClient, room: Room): CommandRecord => {
 
           function error(err: any) {
             let response = `Unable to retrieve the location data, Error no. ${err.code}: ${err.message}`;
-            if (err.code === 1)
-              response = 'You have denied access to the location for the website.';
+            if (err.code === 1) response = 'You have denied access to the location for Sable.';
             if (err.code === 2)
               response = 'Your device does not have a gps module, or it may not be turned on.';
             sendFeedback(response, room, mx.getSafeUserId());

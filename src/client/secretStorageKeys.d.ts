0bc8dae94005742d05269dd1ec6f4cb832ef6f45
hazre <mail@haz.re>
2026-04-14 20:23:16 +0200

    refactor: convert secret storage keys to typescript

--- src/client/secretStorageKeys.d.ts
+++
@@ -1,8 +0,0 @@
-import type { CryptoCallbacks } from '$types/matrix-sdk';
-
-export function storePrivateKey(keyId: string, privateKey: Uint8Array): void;
-export function clearSecretStorageKeys(): void;
-export const cryptoCallbacks: Pick<
-  CryptoCallbacks,
-  'getSecretStorageKey' | 'cacheSecretStorageKey'
->;
---
+++ src/client/secretStorageKeys.ts
@@ -0,0 +1,52 @@
+import type { CryptoCallbacks } from '$types/matrix-sdk';
+
+const secretStorageKeys = new Map<string, Uint8Array>();
+
+export function storePrivateKey(keyId: string, privateKey: Uint8Array): void {
+  if (privateKey instanceof Uint8Array === false) {
+    throw new Error('Unable to store, privateKey is invalid.');
+  }
+  secretStorageKeys.set(keyId, privateKey);
+}
+
+function hasPrivateKey(keyId: string): boolean {
+  return secretStorageKeys.get(keyId) instanceof Uint8Array;
+}
+
+function getPrivateKey(keyId: string): Uint8Array | undefined {
+  return secretStorageKeys.get(keyId);
+}
+
+export function clearSecretStorageKeys(): void {
+  secretStorageKeys.clear();
+}
+
+const getSecretStorageKey: NonNullable<CryptoCallbacks['getSecretStorageKey']> = async (
+  { keys },
+  name
+) => {
+  void name;
+  const keyIds = Object.keys(keys);
+  const keyId = keyIds.find(hasPrivateKey);
+  if (!keyId) return null;
+  const privateKey = getPrivateKey(keyId);
+  if (!privateKey) return null;
+  return [keyId, privateKey];
+};
+
+const cacheSecretStorageKey: NonNullable<CryptoCallbacks['cacheSecretStorageKey']> = (
+  keyId,
+  keyInfo,
+  privateKey
+) => {
+  void keyInfo;
+  secretStorageKeys.set(keyId, privateKey);
+};
+
+export const cryptoCallbacks: Pick<
+  CryptoCallbacks,
+  'getSecretStorageKey' | 'cacheSecretStorageKey'
+> = {
+  getSecretStorageKey,
+  cacheSecretStorageKey,
+};

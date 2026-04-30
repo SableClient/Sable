90878d8fb3878cc06cd66201cbb5fd408fd5f6f9
7w1 <noreply@sl.sable.moe>
2026-04-29 21:51:38 -0500

    fix local message feedback

--- src/app/utils/sendFeedbackToUser.ts
+++ src/app/utils/sendFeedbackToUser.ts
@@ -1,10 +1,10 @@
-import type { Room } from '$types/matrix-sdk';
-import { DuplicateStrategy, MatrixEvent } from '$types/matrix-sdk';
+import type { Room } from "$types/matrix-sdk";
+import { DuplicateStrategy, MatrixEvent } from "$types/matrix-sdk";
 
 export function sendFeedback(msg: string, room: Room, userId: string) {
   const localNotice = new MatrixEvent({
-    type: 'm.room_message',
-    content: { msgtype: 'm.notice', body: msg },
+    type: "m.room.message",
+    content: { msgtype: "m.notice", body: msg },
     event_id: `~sable-feedback-${Date.now()}`,
     room_id: room.roomId,
     sender: userId,

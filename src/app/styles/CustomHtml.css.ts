d2b313e3772a7589f910ab49107532b49a551aff
Evie Gauthier <evie@gauthier.id>
2026-06-17 16:20:05 -0400

    fix(client): align inline emoji with text

--- src/app/styles/CustomHtml.css.ts
+++ src/app/styles/CustomHtml.css.ts
@@ -269,7 +269,7 @@ export const SystemEmoji = style({
   fontFamily:
     'var(--font-emoji), "Apple Color Emoji", "Segoe UI Emoji", "Noto Color Emoji", sans-serif',
   fontSize: '1em',
-  top: '-0.14em',
+  top: '-0.18em',
 });
 
 export const CustomEmoticon = recipe({
@@ -285,7 +285,7 @@ export const CustomEmoticon = recipe({
       lineHeight: '1em',
       verticalAlign: 'middle',
       position: 'relative',
-      top: '-0.14em',
+      top: '-0.18em',
       borderRadius: config.radii.R300,
     },
   ],

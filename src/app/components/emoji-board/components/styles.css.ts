f9f0acd0eab7c3a7be533a96c25adb2a765ba641
Evie Gauthier <evie@gauthier.id>
2026-06-17 21:52:20 -0400

    fix: increase unicode emoji sizing

--- src/app/components/emoji-board/components/styles.css.ts
+++ src/app/components/emoji-board/components/styles.css.ts
@@ -70,8 +70,8 @@ export const Preview = style({
 export const PreviewEmoji = style([
   DefaultReset,
   {
-    width: toRem(36),
-    height: toRem(36),
+    width: toRem(40),
+    height: toRem(40),
   },
 ]);
 export const PreviewImg = style([
@@ -86,8 +86,8 @@ export const PreviewImg = style([
 export const EmojiGlyph = style([
   DefaultReset,
   {
-    width: toRem(36),
-    height: toRem(36),
+    width: toRem(40),
+    height: toRem(40),
     display: 'inline-flex',
     alignItems: 'center',
     justifyContent: 'center',

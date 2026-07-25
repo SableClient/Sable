02bae86ed4684a398e954eb4958f45447441eb75
kimitsune <kimitsune@singularity-node.net>
2026-07-25 15:33:58 +0200

    Try 2 : phase out deprecated matrix sdk API

--- src/app/hooks/useRoomEncryptionStatus.ts
+++ src/app/hooks/useRoomEncryptionStatus.ts
@@ -1,8 +1,9 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState, useMemo } from 'react';
 import type { Room } from '$types/matrix-sdk';
-import { RoomStateEvent } from '$types/matrix-sdk';
+import { RoomStateEvent, MatrixEvent, EventType } from '$types/matrix-sdk';
 import { useMatrixClient } from '$hooks/useMatrixClient';
 import { useDeviceListChange } from '$hooks/useDeviceList';
+import { getStateEvents } from '$utils/room';
 
 /**
  * Determines the encryption status of a room.
@@ -29,7 +30,7 @@ export function useRoomEncryptionStatus(room: Room | undefined): RoomEncryptionS
   const [status, setStatus] = useState<RoomEncryptionStatus>(() => {
     if (!room) return RoomEncryptionStatus.Unknown;
     // Quick sync check
-    return mx.isRoomEncrypted(room.roomId)
+    return room.hasEncryptionStateEvent()
       ? RoomEncryptionStatus.Encrypted
       : RoomEncryptionStatus.Unencrypted;
   });
@@ -50,7 +51,7 @@ export function useRoomEncryptionStatus(room: Room | undefined): RoomEncryptionS
     } catch {
       // Fallback to sync check
       setStatus(
-        mx.isRoomEncrypted(room.roomId)
+         room.hasEncryptionStateEvent()
           ? RoomEncryptionStatus.Encrypted
           : RoomEncryptionStatus.Unencrypted
       );
@@ -73,3 +74,31 @@ export function useRoomEncryptionStatus(room: Room | undefined): RoomEncryptionS
 
   return status;
 }
+
+/*
+export async function isRoomEncrypted(room: Room, cryptoApi: CryptoApi): Promise<boolean> {
+    return await cryptoApi.isEncryptionEnabledInRoom(room.roomId) && room.hasEncryptionStateEvent();
+}
+
+// Hook to simplify watching whether a Matrix room is encrypted, returns null if room is undefined or the state is loading
+export function useIsEncrypted(room: Room): boolean | null {
+    const mx = useMatrixClient();
+    const [status, setStatus] = useState<RoomEncryptionStatus>(() => {
+      if (!room) return RoomEncryptionStatus.Unknown;
+      // Quick sync check
+      return room.hasEncryptionStateEvent()
+        ? RoomEncryptionStatus.Encrypted
+        : RoomEncryptionStatus.Unencrypted;
+    });
+    const events: MatrixEvent[] = getStateEvents(room, EventType.RoomEncryption);
+
+    return useMemo(
+        async () => {
+            const crypto = mx.getCrypto();
+            if (!room || !crypto) return null;
+
+            return await isRoomEncrypted(room, crypto);
+        },
+        [room, events],
+    );
+}*/

b55b47f00db245aac3f09bdebbf70a4b293adb63
kimitsune <kimitsune@singularity-node.net>
2026-07-25 15:59:07 +0200

    Restore accidently modified file during debugging

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -12,14 +12,22 @@ const langFromUrl = (url: string): string | undefined => {
 };
 
 i18n
+  // i18next-http-backend
+  // loads translations from your server
+  // https://github.com/i18next/i18next-http-backend
   .use(Backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
+  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
   .init<HttpBackendOptions>({
     debug: false,
     fallbackLng: 'en',
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // not needed for react as it escapes by default
     },
     load: 'languageOnly',
     backend: {
